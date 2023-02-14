37b1b0bd7effef40a88870aa507fbea6602b2381
getekj <joannagetek@gmail.com>
2023-02-14 16:12:24 -0500

    added delete queries

--- data_manipulation.sql
+++ data_manipulation.sql
@@ -31,3 +31,8 @@ VALUES (_transfusionID, _patientID, _nurseID, _date, _description, _infusionRate
 -- UPDATE QUERY
 
 -- DELETE QUERY
+DELETE FROM Patients
+WHERE PatientID = _patientID;
+
+DELETE FROM TransfusionOrders
+WHERE TransfusionID = _transfusionID;
