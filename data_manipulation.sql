849797f6979b8b22bb14fdcb7761027259844560
getekj <joannagetek@gmail.com>
2023-02-15 11:48:07 -0500

    added select and update queries for dropdown bars and update forms

--- data_manipulation.sql
+++ data_manipulation.sql
@@ -1,10 +1,16 @@
 -- SELECT QUERIES
+-- Queries to display entire tables
 SELECT * FROM Patients;
 SELECT * FROM Nurses;
 SELECT * FROM BloodProducts;
 SELECT * FROM ProductTypes;
 SELECT * FROM BloodTypes;
 SELECT * FROM TransfusionOrders;
+
+-- Queries to populate nurses and patients on drop down display for patients.html page
+SELECT PatientID, Name FROM Patients;
+SELECT NurseID, Name FROM Nurses;
+
 
 -- INSERT QUERIES
 INSERT INTO BloodProducts (ProductTypeId, BloodTypeID, DrawnDate, ExpirationDate, DonorID, Volume)
@@ -31,7 +37,17 @@ VALUES (_transfusionID, _patientID, _nurseID, _date, _description, _infusionRate
 -- UPDATE QUERY
 UPDATE Patients SET BloodTypeID = NULL WHERE BloodTypeID = _bloodTypeID;
 
--- DELETE QUERY
+-- update patient based on submission of the edit patient form 
+UPDATE Patients 
+SET Name = _name, BirthDate = _birthDate, MedicalRecordNumber = _medicalRecordNumber, BloodTypeID = _bloodTypeID 
+WHERE PatientID = _patientID;
+
+-- update transfusion order based on submission of edit transfusion form
+UPDATE TransfusionOrders
+SET PatientID = _patientID, NurseID = _nurseID, Date = _date, Description = _description, InfusionRate = _infusionRate)
+WHERE TransfusionID = _transfusionID
+
+-- DELETE QUERIES
 DELETE FROM Patients
 WHERE PatientID = _patientID;
 
