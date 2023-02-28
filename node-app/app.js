ff013d1370bcd9ea5bfc8263b46fb1d63dcfb4b2
getekj <joannagetek@gmail.com>
2023-02-28 13:48:39 -0800

    fixed date formatting

--- node-app/app.js
+++ node-app/app.js
@@ -6,10 +6,11 @@
 */
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
+var helpers = require('handlebars-helpers')(); //helper package used to format date
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
-PORT        = 9179;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 39129;                 // Set a port number at the top so it's easy to change in the future
 
 
 // Database
