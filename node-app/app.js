bc8ae7c0c1786848e9d187b834d8404eb6b1e43a
getekj <joannagetek@gmail.com>
2023-02-28 12:38:42 -0800

    added files for layout and database

--- node-app/app.js
+++ node-app/app.js
@@ -3,17 +3,29 @@
 /*
     SETUP
 */
+
+
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
 PORT        = 39129;                 // Set a port number at the top so it's easy to change in the future
 
+// app.js
+
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
+
 /*
     ROUTES
 */
-app.get('/', function(req, res)                 // This is the basic syntax for what is called a 'route'
+// app.js
+
+app.get('/', function(req, res)
     {
-        res.send("The server is running!")      // This function literally sends the string "The server is running!" to the computer
-    });                                         // requesting the web site.
+        res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+    });                                         // will process this file, before sending the finished HTML to the client.                                        // requesting the web site.
 
 /*
     LISTENER
