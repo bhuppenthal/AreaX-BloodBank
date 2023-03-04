55674bb164eb556f0f10f92a21be6c092501908d
getekj <joannagetek@gmail.com>
2023-03-04 13:54:50 -0800

    blood types add form working

--- node-app/app.js
+++ node-app/app.js
@@ -397,6 +397,47 @@ app.post('/add-blood-product-ajax', function(req, res) {
 });
 
 /*
+    BLOOD TYPES FORMS
+*/ 
+app.post('/add-blood-type-ajax', function(req, res) 
+{
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO BloodTypes (BloodTypeID) VALUES ('${data.BloodTypeID}');`;
+
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            // If there was no error, perform a SELECT * on Nurses
+            query2 = `SELECT * FROM BloodTypes;`;
+            db.pool.query(query2, function(error, rows, fields){
+
+                // If there was an error on the second query, send a 400
+                if (error) {
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else
+                {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
+
+/*
     LISTENER
 */
 app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
