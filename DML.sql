0e763edcf04e7e2c28879c2e27481f598e424d4b
Brenda Huppenthal <huppentb@oregonstate.edu>
2023-03-20 09:41:08 -0700

    Add comments and clean up.

--- DML.sql
+++ DML.sql
@@ -1,3 +1,10 @@
+/*
+    Team Area X Blood Bankers
+    Group 47
+    Joanna Getek
+    Brenda Huppenthal
+*/
+
 -- SELECT QUERIES
 -- Queries to display entire table
 -- Also displays dropdowns for BloodTypes and ProductsTypes
@@ -16,16 +23,16 @@ SELECT NurseID, Name FROM Nurses;
 SELECT NurseID FROM Nurses WHERE Name = _nurseName;
 SELECT PatientID FROM Patients WHERE Name =_patientName;
 
--- select the current volume of a blood product
+-- Select the current volume of a blood product
 SELECT Volume FROM BloodProductID WHERE BloodProductID = _bloodProductID;
 
--- select the data required to fill out the transfusion orders overview
+-- Select the data required to fill out the transfusion orders overview
 SELECT TransfusionOrders.TransfusionID, Patients.Name AS PatientName, Nurses.Name AS NurseName, TransfusionOrders.Date, TransfusionOrders.Description, TransfusionOrders.InfusionRate 
 FROM TransfusionOrders 
 LEFT JOIN Patients ON EXISTS(SELECT TransfusionOrders.PatientID INTERSECT SELECT Patients.PatientID) 
 LEFT JOIN Nurses ON EXISTS(SELECT TransfusionOrders.NurseID INTERSECT SELECT Nurses.NurseID);
 
--- select the data required to fill out the line by line blood product totals on transfusion orders page for transfusiondetails table
+-- Select the data required to fill out the line by line blood product totals on transfusion orders page for transfusiondetails table
 SELECT TransfusionOrders.TransfusionID, Patients.Name AS PatientName, Nurses.Name AS NurseName, BloodProducts.ProductTypeID, BloodProducts.BloodTypeID, TransfusionDetails.Volume, TransfusionOrders.InfusionRate 
 FROM TransfusionOrders 
 LEFT JOIN Patients ON EXISTS(SELECT TransfusionOrders.PatientID INTERSECT SELECT Patients.PatientID) 
@@ -59,27 +66,27 @@ VALUES (_transfusionID, _patientID, _nurseID, _date, _description, _infusionRate
 -- UPDATE QUERY
 UPDATE Patients SET BloodTypeID = NULL WHERE BloodTypeID = _bloodTypeID;
 
--- update patient based on submission of the edit patient form 
+-- Update patient based on submission of the edit patient form 
 UPDATE Patients 
 SET Name = _name, BirthDate = _birthDate, MedicalRecordNumber = _medicalRecordNumber, BloodTypeID = _bloodTypeID 
 WHERE PatientID = _patientID;
 
--- update patient blood type to null
+-- Update patient blood type to null
 UPDATE Patients
 SET BloodTypeID = NULL
 WHERE PatientID = _patientID;
 
--- update nurses based on submission of edit nurse form
+-- Update nurses based on submission of edit nurse form
 UPDATE Nurses
 SET Extension = _Extension;
 WHERE NurseID = _nurseID;
 
--- update transfusion order based on submission of edit transfusion form
+-- Update transfusion order based on submission of edit transfusion form
 UPDATE TransfusionOrders
 SET PatientID = _patientID, NurseID = _nurseID, Date = _date, Description = _description, InfusionRate = _infusionRate
 WHERE TransfusionID = _transfusionID
 
---update volume of a blood product due to a transfusion order
+-- Update volume of a blood product due to a transfusion order
 UPDATE BloodProducts
 SET Volume = _volume
 WHERE BloodProductID = _bloodproductID;
