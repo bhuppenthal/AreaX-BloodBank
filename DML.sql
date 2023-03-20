ba8988889a813f9ddcacd63a2ff23e72da2f5b57
getekj <joannagetek@gmail.com>
2023-03-19 17:25:43 -0700

    added nurse queries and modified transfusion select queries

--- DML.sql
+++ DML.sql
@@ -1,5 +1,6 @@
 -- SELECT QUERIES
 -- Queries to display entire table
+-- Also displays dropdowns for BloodTypes and ProductsTypes
 SELECT * FROM Patients;
 SELECT * FROM Nurses;
 SELECT * FROM BloodProducts;
@@ -19,18 +20,19 @@ SELECT PatientID FROM Patients WHERE Name =_patientName;
 SELECT Volume FROM BloodProductID WHERE BloodProductID = _bloodProductID;
 
 -- select the data required to fill out the transfusion orders overview
-SELECT TransfusionOrders.TransfusionID, Patients.Name, Nurses.Name, TransfusionOrders.Date, TransfusionOrders.Description, TransfusionOrders.InfusionRate
-FROM TransfusionOrders
-INNER JOIN Patients on TransfusionOrders.PatientID = Patients.PatientID
-INNER JOIN Nurses ON TransfusionOrders.NurseID = Nurses.NurseID;
+SELECT TransfusionOrders.TransfusionID, Patients.Name AS PatientName, Nurses.Name AS NurseName, TransfusionOrders.Date, TransfusionOrders.Description, TransfusionOrders.InfusionRate 
+FROM TransfusionOrders 
+LEFT JOIN Patients ON EXISTS(SELECT TransfusionOrders.PatientID INTERSECT SELECT Patients.PatientID) 
+LEFT JOIN Nurses ON EXISTS(SELECT TransfusionOrders.NurseID INTERSECT SELECT Nurses.NurseID);
 
--- select the data required to fill out the line by line blood product totals on transfusion orders page
-SELECT TransfusionOrders.TransfusionID, Patients.Name AS PatientName, Nurses.Name AS NurseName, BloodProducts.ProductTypeID, BloodProducts.BloodTypeID, TransfusionDetails.Volume, TransfusionOrders.InfusionRate
-FROM TransfusionOrders
-INNER JOIN Patients ON TransfusionOrders.PatientID = Patients.PatientId
-INNER JOIN Nurses ON TransfusionOrders.NurseId = Nurses.NurseID
-INNER JOIN TransfusionDetails ON TransfusionOrders.TransfusionID = TransfusionDetails.TransfusionID
-INNER JOIN BloodProducts ON TransfusionDetails.BloodProductID = BloodProducts.BloodProductID;
+-- select the data required to fill out the line by line blood product totals on transfusion orders page for transfusiondetails table
+SELECT TransfusionOrders.TransfusionID, Patients.Name AS PatientName, Nurses.Name AS NurseName, BloodProducts.ProductTypeID, BloodProducts.BloodTypeID, TransfusionDetails.Volume, TransfusionOrders.InfusionRate 
+FROM TransfusionOrders 
+LEFT JOIN Patients ON EXISTS(SELECT TransfusionOrders.PatientID INTERSECT SELECT Patients.PatientID) 
+LEFT JOIN Nurses ON EXISTS(SELECT TransfusionOrders.NurseID INTERSECT SELECT Nurses.NurseID) 
+INNER JOIN TransfusionDetails ON TransfusionOrders.TransfusionID = TransfusionDetails.TransfusionID 
+INNER JOIN BloodProducts ON TransfusionDetails.BloodProductID = BloodProducts.BloodProductID 
+ORDER BY TransfusionOrders.TransfusionID ASC;
 
 -- INSERT QUERIES
 INSERT INTO BloodProducts (ProductTypeId, BloodTypeID, DrawnDate, ExpirationDate, DonorID, Volume)
@@ -67,6 +69,11 @@ UPDATE Patients
 SET BloodTypeID = NULL
 WHERE PatientID = _patientID;
 
+-- update nurses based on submission of edit nurse form
+UPDATE Nurses
+SET Extension = _Extension;
+WHERE NurseID = _nurseID;
+
 -- update transfusion order based on submission of edit transfusion form
 UPDATE TransfusionOrders
 SET PatientID = _patientID, NurseID = _nurseID, Date = _date, Description = _description, InfusionRate = _infusionRate
@@ -80,6 +87,9 @@ WHERE BloodProductID = _bloodproductID;
 -- DELETE QUERIES
 DELETE FROM Patients
 WHERE PatientID = _patientID;
+
+DELETE FROM Nurses
+WHERE NurseID = _nurseID;
 
 DELETE FROM TransfusionOrders
 WHERE TransfusionID = _transfusionID;
